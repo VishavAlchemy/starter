718217c03b1197896930ab7e3afd2a9293c722a1
VishavAlchemy <infinitevishav@gmail.com>
2024-12-20 11:06:57 -0500

    Update page.tsx

--- app/brand-genesis/page.tsx
+++ app/brand-genesis/page.tsx
@@ -3,7 +3,7 @@ import Features from '@/components/7dc/features'
 import Cta from '@/components/7dc/cta'
 import FeatureTwo from '@/components/7dc/featuretwo'
 import React from 'react'
-import { Main, Section, Container } from "@/components/craft";
+import { Main } from "@/components/craft";
 
 
 const page = () => {
